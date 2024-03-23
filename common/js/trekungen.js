6a22f56850f752233a62ac83ba20a98b0c795ec7
jeffyactive <jeff@reelyactive.com>
2024-03-22 22:08:09 -0400

    Created core map visualisation with OpenLayers

---
+++ common/js/trekungen.js
@@ -0,0 +1,26 @@
+// Adapted from https://openlayers.org/en/latest/examples/geojson.html
+
+const GeoJSON = ol.format.GeoJSON;
+
+const vectorSource = new ol.source.Vector({});
+
+fetch('../../denvegaser/geojson/overview.geojson')
+    .then((response) => response.json())
+    .then((json) => vectorSource.addFeatures(new GeoJSON().readFeatures(json)));
+
+const vectorLayer = new ol.layer.Vector({ source: vectorSource });
+
+const map = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM(),
+    }),
+    vectorLayer
+  ],
+  target: 'map',
+  view: new ol.View({
+    projection: 'EPSG:4326',
+    center: [-73.55413, 45.50882],
+    zoom: 2
+  }),
+});
